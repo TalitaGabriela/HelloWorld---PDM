985030e523aba0fd866849ce7f90f53c44fb5efc
TalitaGabriela <talitagabriela1216@gmail.com>
2024-03-01 17:32:13 -0400

    Commit
    Att css

--- App.js
+++ App.js
@@ -4,17 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <h1 style={styles.text}>Curso Técnico Integrado em Informática Perfil:</h1>
-      <p style={styles.text}>Desenvolver programas de computador seguindo as especificações da lógica e das linguagens de programacação. É o que aprende o aluno do curso técnico em informática que, depois de formado, estará apto a instalar sistemas operacionais, aplicativos e periféricos, desenvolver aplicações com acesso a web e banco de dados, realizar manutenção de computadores e instalar redes em locais de pequeno porte.</p>
+      <Text style={styles.textTitle}>Curso Técnico Integrado em Informática Perfil:</Text>
+      <Text style={styles.text}> Desenvolver programas de computador seguindo as especificações da lógica e das linguagens de programacação. É o que aprende o aluno do curso técnico em informática que, depois de formado, estará apto a instalar sistemas operacionais, aplicativos e periféricos, desenvolver aplicações com acesso a web e banco de dados, realizar manutenção de computadores e instalar redes em locais de pequeno porte.</Text>
+     
+      <Text style={styles.textTitle}>Duração:</Text>
+      <Text style={styles.text}>Duração: 3 anos (6 semestres)</Text>
+     
+      <Text style={styles.textTitle}>Carga Horária:</Text>
+      <Text style={styles.text}>3.380 horas</Text>
       
-      <h1 style={styles.text}>Duração:</h1>
-      <p style={styles.text}>3 anos(6 semestres)</p>
-
-      <h1 style={styles.text}>Carga Horária:</h1>
-      <p style={styles.text}>3.380 horas</p>
-
-      <h1 style={styles.text}>Saiba mais em:</h1>
-      <p style={styles.text}>https://www.ifms.edu.br/campi/campus-nova-andradina/cursos/integrado/informatica</p>
+      <Text style={styles.textTitle}>Saiba mais em</Text>
+      <Text style={styles.text}>https://www.ifms.edu.br/campi/campus-nova-andradina/cursos/integrado/informatica</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -30,5 +30,11 @@ const styles = StyleSheet.create({
   text:{
     fontFamily: 'Arial',
     color: 'white',
+  },
+  textTitle:{
+    fontFamily: 'Arial',
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
   }
 });
