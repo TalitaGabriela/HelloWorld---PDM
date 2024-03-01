ed30868ef16d05ca73da56b2c06fad8afd0f97dd
TalitaGabriela <talitagabriela1216@gmail.com>
2024-03-01 17:17:21 -0400

    Commit
    Espaços

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-
     <View style={styles.container}>
       <h1 style={styles.text}>Curso Técnico Integrado em Informática Perfil:</h1>
       <p style={styles.text}>Desenvolver programas de computador seguindo as especificações da lógica e das linguagens de programacação. É o que aprende o aluno do curso técnico em informática que, depois de formado, estará apto a instalar sistemas operacionais, aplicativos e periféricos, desenvolver aplicações com acesso a web e banco de dados, realizar manutenção de computadores e instalar redes em locais de pequeno porte.</p>
@@ -18,7 +17,6 @@ export default function App() {
       <p style={styles.text}>https://www.ifms.edu.br/campi/campus-nova-andradina/cursos/integrado/informatica</p>
       <StatusBar style="auto" />
     </View>
-    
   );
 }
 
